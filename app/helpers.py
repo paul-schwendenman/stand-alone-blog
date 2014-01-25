d4ce4c73a344ec994f73039386fa3ba304bf3e3d
Paul Schwendenman <schwendenman.paul@gmail.com>
2014-01-25 15:41:27 -0500

    Set up apps

---
+++ app/helpers.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext
+import json
+
+def render_to_json(func):
+    def wrapper(request, *args, **kw):
+        response_data = func(request, *args, **kw)
+
+        return HttpResponse(json.dumps(response_data),
+            content_type="application/json")
+    return wrapper
+
+def render_to_template(template):
+    def real_decorator(func):
+        def wrapper(request, *args, **kw):
+            response_data = func(request, *args, **kw)
+           
+            if isinstance(response_data, dict):
+                response_data['user__'] = request.user
+                response_data['params'] = request.REQUEST
+           
+                return render_to_response(template, response_data,
+                            context_instance=RequestContext(request))
+            elif isinstance(response_data, HttpResponse):
+                return response_data
+        return wrapper
+    return real_decorator
--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from helpers import render_to_template, render_to_json
 
-# Create your views here.
+@render_to_json
+#@render_to_template('index.html')
+def index(request):
+    return {'body': 'index'}
+
+def post(request, post_id):
+    return {
+        'body': 'post',
+        'id': post_id,
+    }
+
