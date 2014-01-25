43eb008b2e49ebe3540a9b5028a32a5760f6a7f3
Paul Schwendenman <schwendenman.paul@gmail.com>
2014-01-25 15:19:23 -0500

    Added the model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    slug = models.SlugField(editable=False)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+	if not self.id:
+	    self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.title
+
