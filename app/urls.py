2fc942da2d271cc2b84c4cdf2a9b4ecd0324238c
Paul Schwendenman <schwendenman.paul@gmail.com>
2014-01-25 15:08:56 -0500

    Added urls

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
+urlpatterns = patterns('app.views',
     # Examples:
     # url(r'^$', 'blog.views.home', name='home'),
     # url(r'^blog/', include('blog.foo.urls')),
@@ -13,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include(admin.site.urls)),
+    url(r'^$', 'index'),
+    url(r'^(?P<post_id>\d+)/$', 'post'),
 )
