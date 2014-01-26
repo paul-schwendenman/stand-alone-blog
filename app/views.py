68095290777dfe45184a3e957eff1f248b38db03
Paul Schwendenman <schwendenman.paul@gmail.com>
2014-01-26 13:43:48 -0500

    Got templates working

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from helpers import render_to_template_json, render_to_json
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from models import Post
+import datetime
 
-#@render_to_json
 @render_to_template_json('index.html')
 def index(request):
     posts = list(Post.objects.all().values('title', 'body', 'slug'))
@@ -10,17 +12,19 @@ def index(request):
         posts = None    
 
     return {
-        'body': 'index',
+        'template': 'index.html',
         'posts': posts,
     }
 
-@render_to_json
+@render_to_template_json('blog.html')
 def post(request, post_slug):
     post = get_object_or_404(Post, slug=post_slug)
     return {
+        'template': 'blog.html',
         'title': post.title,
         'body': post.body,
-        'created': str(post.created),
+        'created_time': post.created.strftime("%I:%M %p"),
+        'created_date': post.created.strftime("%b %d, %Y"),
         'slug': post_slug,
     }
 
